4716026935eb20fab05c282b46b7531e06a7fc63
Jeevaraj1602 <jeevaraj1627@gmail.com>
2026-09-02 14:43:04 +0530

    added feature

--- Factorial.java
+++ Factorial.java
@@ -13,6 +13,6 @@ public class Factorial {
             factorial = factorial * i;
         }
 
-        System.out.println("Factorial answer = " + factorial);
+        System.out.println("Factorial = " + factorial);
     }
 }
